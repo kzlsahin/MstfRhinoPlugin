30a8bf037d13a588c3498ace4d3335fe4c4c34cc
Mustafa Şentürk <46689277+kzlsahin@users.noreply.github.com>
2022-07-19 14:18:51 +0300

    license name added

--- MstfRhinoPlugin1Command.cs
+++ MstfRhinoPlugin1Command.cs
@@ -1,6 +1,8 @@
 /*
 * The MIT License (MIT)
 *
+* Mustafa Şentürk - 2022
+*
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
 * in the Software without restriction, including without limitation the rights
